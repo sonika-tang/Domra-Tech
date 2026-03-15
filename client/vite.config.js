a94b0355fb26f488e0b9c15d3385d53607c7c60f
Sonika Tang <sonikatang630@gmail.com>
2026-03-15 08:29:23 +0700

    Update API proxy configuration in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // '/api': 'http://localhost:3002'
-      '/api': 'https://domra-tech-production-3f20.up.railway.app' // Forward all requests starting with /api
+      '/api': 'http://localhost:3000'
+      // '/api': 'https://domra-tech-production-3f20.up.railway.app' // Forward all requests starting with /api
     }
     // proxy: {
     //   "/api":{
